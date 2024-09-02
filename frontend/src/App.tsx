8bc16763cdd016cf1937f771f4f0e24b12c4d750
Tomiwa Adelae <thetommedia@gmail.com>
2024-09-02 09:52:31 +0100

    Implemented the initials setup of the project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+	return (
+		<>
+			<h1 className="font-bold text-3xl text-blue-400">
+				Hey there! I am ready to ace this interview and land this job!
+			</h1>
+			<Button>Click me</Button>
+		</>
+	);
+}
+
+export default App;
